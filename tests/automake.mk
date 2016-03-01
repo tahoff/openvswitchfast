2c793a77896dab4530aa39ca54eef1d284cb60b3
Adam Hoff <timothy_hoff@brown.edu>
2016-03-01 20:34:34 +0000

    Added learn_learn testing

--- tests/automake.mk
+++ tests/automake.mk
@@ -26,6 +26,7 @@ TESTSUITE_AT = \
 	tests/lacp.at \
 	tests/learn.at \
 	tests/learn_delete.at \
+	tests/learn_learn.at \
 	tests/increment_table_id.at \
     tests/vconn.at \
 	tests/file_name.at \
